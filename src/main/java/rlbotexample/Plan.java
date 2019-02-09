51c40e26e035ab9503776778c8b90268f0294c5e
Linus <linusny@hotmail.com>
2019-02-09 14:33:52 +0100

    first commit

---
+++ src/main/java/rlbotexample/Plan.java
@@ -0,0 +1,37 @@
+package rlbotexample;
+
+import rlbot.Bot;
+import rlbotexample.input.DataPacket;
+import rlbotexample.output.ControlsOutput;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Plan {
+
+    private final List<Skill> steps = new ArrayList<>();
+    private int index;
+
+    public Plan() {
+    }
+
+    public Plan withStep(Skill skill) {
+        this.steps.add(skill);
+        return this;
+    }
+
+    public ControlsOutput getOutput(DataPacket packet, Bot bot) {
+
+        while (index < steps.size()) {
+            Skill currentSkill = steps.get(index);
+            ControlsOutput output = currentSkill.getOutput(packet, bot);
+
+            if (output != null) {
+                return output;
+            }
+            index++;
+        }
+
+        return null;
+    }
+}
---
+++ src/main/java/rlbotexample/SampleBot.java
@@ -0,0 +1,289 @@
+package rlbotexample;
+
+import com.sun.jna.platform.unix.Resource;
+import rlbot.Bot;
+import rlbot.ControllerState;
+import rlbot.cppinterop.RLBotDll;
+import rlbot.flat.BallPrediction;
+import rlbot.flat.FieldInfo;
+import rlbot.flat.GameTickPacket;
+import rlbot.flat.QuickChatSelection;
+import rlbot.manager.BotLoopRenderer;
+import rlbot.render.Renderer;
+import rlbotexample.boost.BoostManager;
+import rlbotexample.boost.BoostPad;
+import rlbotexample.dropshot.DropshotTile;
+import rlbotexample.dropshot.DropshotTileManager;
+import rlbotexample.dropshot.DropshotTileState;
+import rlbotexample.input.BallData;
+import rlbotexample.input.CarData;
+import rlbotexample.input.DataPacket;
+import rlbotexample.output.ControlsOutput;
+import rlbotexample.vector.Vector2;
+import rlbotexample.vector.Vector3;
+
+import java.awt.*;
+import java.io.IOException;
+
+public class SampleBot implements Bot {
+    Plan plan;
+    private final int playerIndex;
+    private static final float BALL_RADIUS = 92.75F;
+    public SampleBot(int playerIndex) {
+        this.playerIndex = playerIndex;
+    }
+
+    /**
+     * This is where we keep the actual bot logic. This function shows how to chase the ball.
+     * Modify it to make your bot smarter!
+     */
+    private ControlsOutput processInput(DataPacket input) {
+
+        Vector2 ballPosition = input.ball.position.flatten();
+
+        CarData myCar = input.car;
+        Vector2 carPosition = myCar.position.flatten();
+        Vector2 carDirection = myCar.orientation.noseVector.flatten();
+
+        // Subtract the two positions to get a vector pointing from the car to the ball.
+        Vector2 carToBall = ballPosition.minus(carPosition);
+
+        // How far does the car need to rotate before it's pointing exactly at the ball?
+        double steerCorrectionRadians = carDirection.correctionAngle(carToBall);
+
+        boolean goLeft = steerCorrectionRadians > 0;
+        boolean goRight = steerCorrectionRadians > 0;
+        // This is optional!
+        drawDebugLines(input, myCar, goLeft);
+        Renderer renderer = BotLoopRenderer.forBotLoop(this);
+        try {
+            BallPrediction ballPrediction = RLBotDll.getBallPrediction();
+            ballPrediction.toString();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+        try {
+            if (RLBotDll.getFlatbufferPacket().players(input.car.team).isDemolished()){
+                RLBotDll.sendQuickChat(input.car.team, false, QuickChatSelection.Apologies_Cursing);
+
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        // This is also optional!
+
+        while(true) {
+            if (plan != null) {
+                ControlsOutput planOutput = plan.getOutput(input, this);
+                if (planOutput == null) {
+                    plan = null;
+                } else {
+                    return planOutput;
+                }
+            }
+
+            boolean isKickoff = input.ball.velocity.flatten().magnitude() < 1;
+
+            if (isKickoff) {
+                renderer.drawString2d("Kickoff!", Color.white, new Point(10, 200), 2, 2);
+                if (input.car.position.magnitude() < 1100) {
+                    plan = new Plan()
+                            .withStep(new TimedAction(0.1, new ControlsOutput().withJump()))
+                            .withStep(new TimedAction(0.05, new ControlsOutput()))
+                            .withStep(new TimedAction(0.05, new ControlsOutput().withJump().withPitch(-1)));
+                }
+
+                return Steering.steerTowardPosition(input.car, new Vector3())
+                        .withBoost().withThrottle(1);
+            }
+
+            try {
+                renderer.drawString2d("Goal 1 : " + RLBotDll.getFieldInfo().goals(1).location().x(), Color.white, new Point(10, 100), 2, 2);
+                renderer.drawString2d("Goal 2 : " + RLBotDll.getFieldInfo().goals(0).location().x(), Color.white, new Point(10, 300), 2, 2);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+
+            boolean getBoost = (myCar.boost < 10 && input.car.position.flatten().distance(input.ball.position.flatten()) > 1500);
+
+            if (getBoost) {
+                try {
+                    BoostManager.loadFieldInfo(RLBotDll.getFieldInfo());
+                    BoostManager.loadGameTickPacket(RLBotDll.getFlatbufferPacket());
+                    BoostManager.getFullBoosts();
+
+                    for (int index = 0; index < 5; index++) {
+                        if (myCar.position.flatten().distance(BoostManager.getFullBoosts().get(index).getLocation().flatten()) < 1500) {
+                            renderer.drawString2d("Going for boost" + index, Color.lightGray, new Point(10, 300), 2, 2);
+                            return Steering.steerTowardPosition(input.car, BoostManager.getFullBoosts().get(index).getLocation())
+                                    .withThrottle(1).withBoost();
+                        }
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+            }
+
+            Vector3 ballPosition2 = input.ball.position;
+            boolean canPopBall = ballPosition2.z > BALL_RADIUS * 1.5 &&
+                    ballPosition2.z < BALL_RADIUS * 3 &&
+                    ballPosition2.flatten().distance(input.car.position.flatten()) < 50;
+
+
+            if (canPopBall) {
+                renderer.drawString2d("Can pop!", Color.white, new Point(10, 200), 2, 2);
+                plan = new Plan()
+                        .withStep(new TimedAction(0.2, new ControlsOutput().withJump()))
+                        .withStep(new TimedAction(0.05, new ControlsOutput()))
+                        .withStep(new TimedAction(0.05, new ControlsOutput().withJump().withPitch(1)));
+                return plan.getOutput(input, this);
+            }
+
+            boolean canFlickBall = (myCar.hasWheelContact && ballPosition2.z > BALL_RADIUS * 1.5
+                    && ballPosition2.z < BALL_RADIUS * 3
+                    && ballPosition2.flatten().distance(input.car.position.flatten()) < 300
+                    && input.car.velocity.flatten().x > input.ball.velocity.flatten().x);
+            if (canFlickBall) {
+                renderer.drawString2d("Can Flick!", Color.white, new Point(10, 200), 2, 2);
+                plan = new Plan()
+                        .withStep(new TimedAction(0.1, new ControlsOutput().withJump()))
+                        .withStep(new TimedAction(0.05, new ControlsOutput()))
+                        .withStep(new TimedAction(0.05, new ControlsOutput().withJump().withPitch(-1)));
+                return plan.getOutput(input, this);
+            }
+
+
+            boolean canShootBall = (ballPosition2.flatten().distance(input.car.position.flatten()) < 300
+                    && ballPosition2.flatten().distance(input.car.position.flatten()) > -300
+                    && ballPosition2.z < 100
+                    && ballPosition2.z < BALL_RADIUS * 2
+                    && input.car.velocity.flatten().x > input.ball.velocity.flatten().x);
+
+            if (canShootBall) {
+                renderer.drawString2d("Can Shoot!", Color.white, new Point(10, 200), 2, 2);
+                if (input.car.isSupersonic && input.car.hasWheelContact) {
+                    plan = new Plan()
+                            .withStep(new TimedAction(0.04, new ControlsOutput().withJump().withPitch(1)))
+                            .withStep(new TimedAction(0.04, new ControlsOutput().withBoost().withPitch(-1)));
+                    return plan.getOutput(input, this);
+                } else {
+                    plan = new Plan()
+                            .withStep(new TimedAction(0.2, new ControlsOutput().withJump()))
+                            .withStep(new TimedAction(0.02, new ControlsOutput()))
+                            .withStep(new TimedAction(0.04, new ControlsOutput().withJump().withPitch(-1)));
+                    return plan.getOutput(input, this);
+                }
+            }
+
+            boolean toHigh = input.ball.position.z > 700 && ballPosition2.flatten().distance(input.car.position.flatten()) < 300;
+
+            if (toHigh) {
+                renderer.drawString2d("Ball to high!", Color.white, new Point(10, 200), 2, 2);
+                plan = new Plan()
+                        .withStep(new TimedAction(1, new ControlsOutput().withSlide().withThrottle(1).withSteer(goLeft ? -1 : 1)));
+            } else {
+
+
+                if (input.car.boost < 0) {
+
+                        plan = new Plan()
+                                .withStep(new TimedAction(0.2, new ControlsOutput().withJump()))
+                                .withStep(new TimedAction(0.02, new ControlsOutput()))
+                                .withStep(new TimedAction(0.04, new ControlsOutput().withJump().withPitch(-1)));
+                        return plan.getOutput(input, this);
+
+                }
+                else {
+                    return Steering.steerTowardPosition(input.car, ballPosition2)
+                            .withBoost().withThrottle(1);
+                }
+            }
+
+
+
+
+
+
+
+
+
+
+        }
+
+    }
+
+
+
+
+    /**
+     * This is a nice example of using the rendering feature.
+     */
+    private void drawDebugLines(DataPacket input, CarData myCar, boolean goLeft) {
+        // Here's an example of rendering debug data on the screen.
+        Renderer renderer = BotLoopRenderer.forBotLoop(this);
+
+        // Draw a line from the car to the ball
+        renderer.drawLine3d(Color.LIGHT_GRAY, myCar.position, input.ball.position);
+
+        // Draw a line that points out from the nose of the car.
+        renderer.drawLine3d(goLeft ? Color.BLUE : Color.RED,
+                myCar.position.plus(myCar.orientation.noseVector.scaled(150)),
+                myCar.position.plus(myCar.orientation.noseVector.scaled(300)));
+
+        renderer.drawString3d(goLeft ? "left" : "right", Color.WHITE, myCar.position, 2, 2);
+
+        for (DropshotTile tile: DropshotTileManager.getTiles()) {
+            if (tile.getState() == DropshotTileState.DAMAGED) {
+                renderer.drawCenteredRectangle3d(Color.YELLOW, tile.getLocation(), 4, 4, true);
+            } else if (tile.getState() == DropshotTileState.DESTROYED) {
+                renderer.drawCenteredRectangle3d(Color.RED, tile.getLocation(), 4, 4, true);
+            }
+        }
+
+        // Draw a rectangle on the tile that the car is on
+        DropshotTile tile = DropshotTileManager.pointToTile(myCar.position.flatten());
+        if (tile != null) renderer.drawCenteredRectangle3d(Color.green, tile.getLocation(), 8, 8, false);
+    }
+
+
+    @Override
+    public int getIndex() {
+        return this.playerIndex;
+    }
+
+    /**
+     * This is the most important function. It will automatically get called by the framework with fresh data
+     * every frame. Respond with appropriate controls!
+     */
+    @Override
+    public ControllerState processInput(GameTickPacket packet) {
+
+        if (packet.playersLength() <= playerIndex || packet.ball() == null || !packet.gameInfo().isRoundActive()) {
+            // Just return immediately if something looks wrong with the data. This helps us avoid stack traces.
+            return new ControlsOutput();
+        }
+
+        // Update the boost manager and tile manager with the latest data
+        BoostManager.loadGameTickPacket(packet);
+        BoostManager.getSmallBoosts();
+        DropshotTileManager.loadGameTickPacket(packet);
+
+        // Translate the raw packet data (which is in an unpleasant format) into our custom DataPacket class.
+        // The DataPacket might not include everything from GameTickPacket, so improve it if you need to!
+        DataPacket dataPacket = new DataPacket(packet, playerIndex);
+
+        // Do the actual logic using our dataPacket.
+        ControlsOutput controlsOutput = processInput(dataPacket);
+
+        return controlsOutput;
+    }
+
+    public void retire() {
+        System.out.println("Retiring sample bot " + playerIndex);
+    }
+}
